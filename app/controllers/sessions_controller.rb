6ef9343828071ec0b1ef1e9a53e858a51320e196
Chad Ellison <chad.ellison0123@gmail.com>
2016-07-18 05:39:53 -0600

    Authenticate with bcrypt Restrict access of rooms to logged in users

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to rooms_path
+    else
+      flash.now[:notice] = "Invalid login"
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'rooms#show'
+  root "welcome#index"
+
+  get "/rooms", to: "rooms#show"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount ActionCable.server => "/cable"
