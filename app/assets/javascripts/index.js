d8e07c280ced43df65cdc37908b78b534edbde41
Chad Ellison <chad.ellison0123@gmail.com>
2016-07-19 09:57:36 -0600

    Add image

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#create-account").on("click", function() {
+    $(".create-account").show()
+  })
+
+  $("#sign-in").on("click", function() {
+    $(".sign-in").show()
+  })
+})
