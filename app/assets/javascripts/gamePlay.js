f75be77452b2cb21096a6a513441936c5105a07f
Chad Ellison <chad.ellison0123@gmail.com>
2016-07-21 17:16:17 -0600

    adjust buttons to dissappear properly

--- app/assets/javascripts/gamePlay.js
+++ app/assets/javascripts/gamePlay.js
@@ -4,3 +4,29 @@ function hideGamePlayButtons(){
     $(".pregame").fadeOut()
   });
 };
+
+function initiateBet() {
+  $("#current-bet").on("click", function() {
+    $(".bet-form").fadeOut();
+    $(".action").fadeIn();
+  });
+}
+
+function makeBet() {
+  $("#bet").on("click", function() {
+    $(".action").fadeOut()
+    $(".bet-form").fadeIn()
+  })
+  $("#bet").keypress(function(event) {
+    if (event.which == 13) {
+      $(".action").fadeOut()
+      $(".bet-form").fadeIn()
+    }
+  })
+}
+
+function hideButtonById(id) {
+  $("#" + id).on("click", function() {
+    $("#" + id).fadeOut()
+  })
+}
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,30 +1,9 @@
 $(document).ready(function() {
+  createAccount();
+  signIn();
   hideGamePlayButtons();
-
-  $("#bet").on("click", function() {
-    $(".action").fadeOut()
-    $(".bet-form").fadeIn()
-  })
-  $("#bet").keypress(function(event) {
-    if (event.which == 13) {
-      $(".action").fadeOut()
-      $(".bet-form").fadeIn()
-    }
-  })
-
-  $("#current-bet").on("click", function() {
-    $(".bet-form").fadeOut();
-    $(".action").fadeIn();
-  });
-
-  $("#create-account").on("click", function() {
-    $(".create-account").fadeIn();
-    $(".sign-in").fadeOut();
-  });
-
-  $("#sign-in").on("click", function() {
-    $(".sign-in").fadeIn();
-    $(".create-account").fadeOut();
-  });
-
+  hideButtonById("join")
+  hideButtonById("pocket_cards")
+  initiateBet();
+  makeBet();
 });
